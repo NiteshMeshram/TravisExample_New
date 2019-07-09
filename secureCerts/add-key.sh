8939d7eaa3d338c7d915b6a76a3e99f700b338d0
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2019-07-09 16:45:17 +0530

    Modified the script

--- secureCerts/add-key.sh
+++ secureCerts/add-key.sh
@@ -14,11 +14,11 @@ security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 # Add certificates to keychain and allow codesign to access them
-security import $APP_SCRIPT_PATH/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import $APP_SCRIPT_PATH/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import $APP_SCRIPT_PATH/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import secureCerts/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import secureCerts/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import secureCerts/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 
 
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp "$APP_SCRIPT_PATH/$PROFILE_NAME.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
+cp "secureCerts/$PROFILE_NAME.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
